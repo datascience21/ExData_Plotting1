36dec124f0d9f690a7a39de2b9faa5a863192338
pratap1205 <pratap.parne@gmail.com>
2016-02-06 21:54:57 -0600

    Corrected plot4

--- plot4.R
+++ plot4.R
@@ -9,7 +9,7 @@ data$Date <- as.Date(data$Date, "%d/%m/%Y")
 datasubset <- subset(data,data$Date >="2007-02-01" & data$Date <="2007-02-02")
 
 #Concatenating Date and Time columns and conveting the column to Time class
-datasubset$DateTime <-paste(datasubset$Date,datasubset$TIme)
+datasubset$DateTime <-paste(datasubset$Date,datasubset$Time)
 datasubset$DateTime<-strptime(datasubset$DateTime,"%Y-%m-%d %H:%M:%S")
 
 #Saving the plot to plot4.png in working directory
